e757fc004ee028b1e02f7610562f6ea97dd20e1a
Carl <carl@carlossus.com>
2015-06-11 21:34:06 +0800

    Added package.json. Updated typings

--- src/typings/designco-logger/designco-logger.d.ts
+++ src/typings/designco-logger/designco-logger.d.ts
@@ -1,10 +1,6 @@
 declare module "designco-logger" {
-	export = Logger;
-}
-
-interface Logger {
-	info(message: string): void;
-	warn(message: string): void;
-	error(message: string): void;
-	debug(message: string): void;
+	function info(message: string): void;
+	function warn(message: string): void;
+	function error(message: string): void;
+	function debug(message: string): void;
 }
