c8ef7e27cfd5db4e092378d192b14a8ca6e662ec
Carl <carl@carlossus.com>
2015-09-24 20:58:58 +0800

    Replace typings with root index.d.ts module

--- src/typings/longshot/logger.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "ls-logger" {
-	function info(message: string): void;
-	function warn(message: string): void;
-	function error(message: string): void;
-	function debug(message: string): void;
-}
