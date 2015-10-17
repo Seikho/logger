6540c0978193f7005115338b4730951a37c46f27
Carl <carl@carlossus.com>
2015-10-17 19:16:45 +0800

    Implement '.on' api function

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import chalk = require("chalk");
+import events = require('events');
+
+var emitter = new events.EventEmitter();
 
 export function info(message) {
     log("INFO", message, chalk.blue);
@@ -16,7 +19,12 @@ export function debug(message) {
     log("DEBUG", message, chalk.gray);
 }
 
-function log(prefix, message, colour) {
+export function on(event: string, handler: Function) {
+    emitter.on(event, handler);
+}
+
+function log(prefix: string, message: string, colour: Chalk.ChalkChain) {
     var timestamp = new Date().toTimeString().slice(0,8);
+    emitter.emit(prefix.toLowerCase(), message);
     console.log("[%s] %s: %s", timestamp, colour(prefix), message);
 }
