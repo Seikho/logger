f248f629ff1207e3daffb40ef779cb99f3d3a8c7
Carl <carl@carlossus.com>
2015-10-17 19:16:13 +0800

    Update node type defs

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,9 @@
 export function info(message: string): void;
+
 export function warn(message: string): void;
+
 export function error(message: string): void;
+
 export function debug(message: string): void;
+
+export function on(event: string, handler: Function): void;
